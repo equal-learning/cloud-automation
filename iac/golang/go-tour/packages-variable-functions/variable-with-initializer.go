e9b252474bae930078ab9ab13c60c226a1595c17
Chinmay MOHANTA <equal.learning.org@gmail.com>
2021-09-06 14:37:12 +0200

    package, variable and function

---
+++ iac/golang/go-tour/packages-variable-functions/variable-with-initializer.go
@@ -0,0 +1,17 @@
+package main
+
+/*
+VARIABLE INITIALIZERS :
+
+A var declaration can include initializers, one per variable.
+If an initializer is present, the type can be omitted; the variable will take the type of the initializer.
+ */
+
+import "fmt"
+
+var i, j int = 1, 2
+
+func main() {
+	var c, python, java = true, false, "no!"
+	fmt.Println(i, j, c, python, java)
+}
